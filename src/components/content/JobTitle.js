9d2f59da85e4637e7fe3007ed77cb50e65929f52
khachatryantigran <tigran9202@gmail.com>
2023-01-14 18:15:02 +0400

    commit

---
+++ src/components/content/JobTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JobTitle = () => {
+  return (
+    <div className="jobInfo">
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default JobTitle;
---
+++ src/components/filter/Clear.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Clear = () => {
+  return (
+    <div>
+      <div class="col-lg-12">
+        <a href="/en/jobs" class="green_color pull-right jobClearFilter">
+          Clear filters{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Clear;
