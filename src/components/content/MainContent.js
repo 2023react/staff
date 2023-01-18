4c30e59fc40cf1ad8d4e3f85d37ce2a6e92e6143
khachatryantigran <tigran9202@gmail.com>
2023-01-18 22:38:39 +0400

    jobs Added

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -1,12 +1,29 @@
 import React from "react";
 import JobTitle from "./JobTitle";
+import Navbar from "./Navbar";
+import styles from "./contents.module.scss";
+import { useLocation } from "react-router";
 
 const MainContent = () => {
+  const location = useLocation().pathname;
+
   return (
-    <div>
-      <div>hot jobs</div>
-      <div>navbar</div>
-      <JobTitle />
+    <div className={styles.mainContent}>
+      <div>HOT JOBS CAROUSEL</div>
+
+      <div>
+        {location === "/jobs" ? (
+          <Navbar />
+        ) : (
+          <p className={"styles."}>
+            1 - 50 company results from 436 total companies on staff.am
+          </p>
+        )}
+      </div>
+
+      <div className={styles.jobsColections}>
+        <JobTitle />
+      </div>
     </div>
   );
 };
