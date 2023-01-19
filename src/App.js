954a43b838c95c01ec64c893dc5b3dcd6b92ad4c
khachatryantigran <tigran9202@gmail.com>
2023-01-20 02:48:57 +0400

    asd

--- src/App.js
+++ src/App.js
@@ -5,26 +5,19 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import "./App.scss";
-import CarouselComponent from "./components/content/Carousel";
 import { Layout } from "./components/Layout";
- 
 import Componies from "./pages/Componies";
- 
 import Home from "./pages/Home";
 import Jobs from "./pages/Jobs";
-
 const r = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="/jobs" element={<Jobs />} />
- 
       <Route path="/companies" element={<Componies />} />
- 
     </Route>
   )
 );
-
 function App() {
   return (
     <div className="App">
